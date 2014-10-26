61aec946af774dfe376b3b5edde2a9b32c5c6aef
Adam Boas <anboas@gmail.com>
2014-10-26 16:05:06 -0400

    Initial Commit - Added Assignment 02

---
+++ ITS2592 Assignments/Assignment02/Assignment02/ANB_P01.java
@@ -0,0 +1,48 @@
+package Assignment02;
+
+/***********************************************************************
+'Project:           Assignment 02
+'Programmer:     	Adam Boas
+'Company Info:  	anboas@gmail.com (240) 561-8173
+'Date:              Sept 15, 2014
+'Description:      	Problem Number 01 Chapter 16
+'    
+'	Sorting Objects with the Insertion Sort Algorithm
+'
+'	The insertionSort method in the IntInsertionSorter class presented 
+'	in this chapter sorts an array of int values. Create an 
+'	ObjectInsertionSorter class that can sort Comparable objects. 
+'	Demonstrate the class in a program that sorts an array of 
+'	String objects.
+'
+'  HONOR CODE: I pledge that this program represents my own program code.
+'  I received help from NO ONE in designing and debugging my program.
+'
+'**********************************************************************/
+
+public class ANB_P01 {
+
+	public static void main(String[] args) {
+		
+		// Strings for sorting
+		String[] inputs = { "photograph", "taken", "from", "Houston", "Texas",
+				"juxtaposes", "Earth", "oldest", "satellite", "with", "one",
+				"of", "its", "youngest" };
+
+		// Display unaltered array
+		System.out.println("Original Order: ");
+		for (String element : inputs) {
+			System.out.println("\t" + element + " ");
+		}
+
+		// Sort array
+		System.out.println("\n");
+		ObjectInsertionSorter.ObjectInsertionSort(inputs);
+
+		// Display altered array
+		System.out.println("Sorted Order: ");
+		for (String element : inputs) {
+			System.out.println("\t" + element + " ");
+		}
+	}
+}
---
+++ ITS2592 Assignments/Assignment02/Assignment02/ANB_P02.java
@@ -0,0 +1,64 @@
+package Assignment02;
+
+/***********************************************************************
+'Project:           Assignment 02
+'Programmer:     	Adam Boas
+'Company Info:  	anboas@gmail.com (240) 561-8173
+'Date:              Sept 15, 2014
+'Description:      	Problem Number 03 Chapter 16
+'    
+'	Searching for Objects with the Binary Search Algorithm
+'
+'	The IntBinarySearcher class presented in this chapter searches an int
+'	array for a specific value. Create an ObjectBinarySearcher class that 
+'	can search an array of Comparable objects. Demonstrate the class in a 
+'	program that searches for a String in an array of String objects.
+'
+'  HONOR CODE: I pledge that this program represents my own program code.
+'  I received help from NO ONE in designing and debugging my program.
+'
+'**********************************************************************/
+
+public class ANB_P02 {
+	public static void main(String[] args) {
+
+		// Strings for Sorting
+		String[] inputs = { "MARY", "PATRICIA", "LINDA", "BARBARA",
+				"ELIZABETH", "JENNIFER", "MARIA", "SUSAN", "MARGARET",
+				"DOROTHY", "LISA", "NANCY", "KAREN", "BETTY", "HELEN",
+				"SANDRA", };
+		
+		// String to search for within inputs
+		String searchTerm = "LISA";
+		
+		// Integer value of where the string was found in the array
+		int searchTermFoundIndex;
+
+		// Display the unaltered array
+		System.out.println("\nOriginal Order: ");
+		for (int i = 0; i < inputs.length; i++) {
+			System.out.println(i + "\t" + inputs[i]);
+		}
+		
+		// Sort array
+		ObjectInsertionSorter.ObjectInsertionSort(inputs);
+
+		// Display altered array
+		System.out.println("\nSorted Order: ");
+		for (int i = 0; i < inputs.length; i++) {
+			System.out.println(i + "\t" + inputs[i]);
+		}
+		
+		// Search array for searchTerm and store index from altered array
+		searchTermFoundIndex = ObjectBinarySearcher.search(inputs, searchTerm);
+
+		// If the search term was found, inform us, if not, inform us
+		if (searchTermFoundIndex >= 0) {
+			System.out.println("\n" + searchTerm + " was found at: "
+					+ searchTermFoundIndex);
+		} else {
+			System.out.println("\n" + searchTerm
+					+ " was not found in the array.");
+		}
+	}
+}
