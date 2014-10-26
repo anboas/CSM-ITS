19d7e2e025bfb19903c27f23b5879306d6915de7
Adam Boas <anboas@gmail.com>
2014-10-26 16:06:56 -0400

    Initial Commit - Added Assignment 01

---
+++ ITS2592 Assignments/Assignment01/Assignment01/ANB_P03.java
@@ -0,0 +1,30 @@
+package Assignment01;
+
+
+
+/***********************************************************************
+'Project:           Assignment 01
+'Programmer:     	Adam Boas
+'Company Info:  	anboas@gmail.com (240) 561-8173
+'Date:              Sept 14, 2014
+'Description:      	Problem Number 01 Chapter 15
+'    
+'	Recursive Multiplication
+'
+'	Write a recursive functionii that accepts two arguements into the 
+'	parameters x and y. The function should return the value of x times
+'	y. Remember, multiplication can be performed as repeating addition
+'	as follows:
+		7 * 4 = 4 + 4 + 4 + 4 + 4 + 4 + 4
+'
+'  HONOR CODE: I pledge that this program represents my own program code.
+'  I received help from NO ONE in designing and debugging my program.
+'
+'**********************************************************************/
+
+public class ANB_P03 {
+
+	public static void main(String[] args) {
+		System.out.println(Recursive.multiply(6,9));
+	}
+}
---
+++ ITS2592 Assignments/Assignment01/Assignment01/Recursive.java
@@ -0,0 +1,26 @@
+package Assignment01;
+
+
+public class Recursive {
+
+	/**
+	 * Multiply two numbers together by recursively adding them
+	 * @param x First number
+	 * @param y Second number
+	 * @return
+	 */
+	public static int multiply(int x, int y) {
+		// Handles multiplying by 0 (5 * 0)
+		if (x == 0 || y == 0) {
+			return 0;
+			
+		// Handles multiplying by negatives (5 * -4)
+		} else if (x < 0) {
+			return -x + multiply(x, y + 1);
+			
+		// Handles default operation (5 * 4)
+		} else {
+			return x + multiply(x, y - 1);
+		}
+	}
+}
