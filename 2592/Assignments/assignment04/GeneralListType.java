e25d4bca29ed6d0f02cb493b54b113b9a6109e9c
Adam Boas <anboas@gmail.com>
2014-11-05 19:15:52 -0500

    Initial Commit of GeneralListType

---
+++ 2592/Assignments/assignment04/GeneralListType.java
@@ -0,0 +1,75 @@
+package assignment04;
+
+public class GeneralListType<E> implements GeneralList<E> {
+	// Constants for the default capacity and
+	// the resizing factor.
+	private final int DEFAULT_CAPACITY = 10;
+	private final int RESIZE_FACTOR = 2;
+
+	@Override
+	public void add(E element) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void add(int index, E element) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean contains(E element) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public E get(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int indexOf(E element) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean remove(E element) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public E remove(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public E set(int index, E element) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
