6d1160f7e06fb94a77c48fea6e92be0cd6db5a53
Adam Boas <anboas@gmail.com>
2014-11-05 19:21:42 -0500

    Added Default Constructor, add(index, element), clear() and resize()

--- 2592/Assignments/assignment04/GeneralListType.java
+++ 2592/Assignments/assignment04/GeneralListType.java
@@ -1,43 +1,82 @@
 package assignment04;
 
-public class GeneralListType<E> implements GeneralList<E> {
+public class GeneralListType implements GeneralList<Object> {
 	// Constants for the default capacity and
 	// the resizing factor.
 	private final int DEFAULT_CAPACITY = 10;
 	private final int RESIZE_FACTOR = 2;
 
-	@Override
-	public void add(E element) {
-		// TODO Auto-generated method stub
+	// Private Fields
+	private Object[] list; // The List
+	private int elements; // Number of elements stored
 
+	/**
+	 * This constructor creates an empty list of the default capacity.
+	 */
+	public GeneralListType() {
+		list = new Object[DEFAULT_CAPACITY];
+		elements = 0;
 	}
 
 	@Override
-	public void add(int index, E element) {
-		// TODO Auto-generated method stub
+	public void add(Object element) {
+		// If the list is full, resize it.
+		if (elements == list.length) {
+			resize();
+		}
 
+		// Add str to the end of the list.
+		list[elements] = element;
+
+		// Adjust the number of elements.
+		elements++;
+	}
+
+	@Override
+	public void add(int index, Object element) {
+		// First make sure the index is valid.
+		if (index >= elements || index < 0)
+			throw new IndexOutOfBoundsException();
+
+		// If the list is full, resize it.
+		if (elements == list.length) {
+			resize();
+		}
+
+		// Shift the elements starting at index
+		// to the right one position.
+		for (int index2 = elements; index2 > index; index2--)
+			list[index2] = list[index2 - 1];
+
+		// Add the new element at index.
+		list[index] = element;
+
+		// Adjust the number of elements.
+		elements++;
 	}
 
 	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
-
+		for (int index = 0; index < list.length; index++) {
+			list[index] = null;
+		}
+		elements = 0;
 	}
 
 	@Override
-	public boolean contains(E element) {
+	public boolean contains(Object element) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public E get(int index) {
+	public Object get(int index) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public int indexOf(E element) {
+	public int indexOf(Object element) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
@@ -49,19 +88,19 @@ public class GeneralListType<E> implements GeneralList<E> {
 	}
 
 	@Override
-	public boolean remove(E element) {
+	public boolean remove(Object element) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public E remove(int index) {
+	public Object remove(int index) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public E set(int index, E element) {
+	public Object set(int index, Object element) {
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -70,6 +109,25 @@ public class GeneralListType<E> implements GeneralList<E> {
 	public int size() {
 		// TODO Auto-generated method stub
 		return 0;
+	}
+
+	/**
+	 * Resizes the list to twice its current length.
+	 */
+	private void resize() {
+		// Calculate the new length, which is the current
+		// length multiplied by the resizing factor.
+		int newLength = list.length * RESIZE_FACTOR;
+
+		// Create a new list.
+		Object[] tempList = new String[newLength];
+
+		// Copy the existing elements to the new list.
+		for (int index = 0; index < elements; index++)
+			tempList[index] = list[index];
+
+		// Replace the existing list with the new one.
+		list = tempList;
 	}
 
 }
