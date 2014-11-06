30357d07883fc8cc9bd24918bace8e1e4a8d5db1
Adam Boas <anboas@gmail.com>
2014-11-05 19:29:42 -0500

    Initial Commit of GeneralListType driver class

--- 2592/Assignments/assignment04/ANB_P01.java
+++ 2592/Assignments/assignment04/ANB_P01.java
@@ -1,5 +1,7 @@
 package assignment04;
 
+import java.util.Scanner;
+
 /***********************************************************************
 'Project: 			Assignment 04
 'Programmer: 		Adam Boas
@@ -21,7 +23,58 @@ package assignment04;
 
 public class ANB_P01 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
+		// Create a StringListType object.
+		GeneralListType myGeneralList = new GeneralListType();
 
+		// Create a Scanner for keyboard input.
+		Scanner keyboard = new Scanner(System.in);
+
+		// Get a line of input.
+		System.out.println("Enter a series of words:");
+		String input = keyboard.nextLine();
+
+		// Tokenize the GeneralList.
+		Object[] tokens = input.split(" ");
+
+		// Add each of the tokens to the list.
+		for (Object s : tokens)
+			myGeneralList.add(s);
+
+		// Display the list.
+		System.out.println("\nHere are the words you entered:");
+		displayList(myGeneralList);
+
+		// Remove the first and last elements.
+		myGeneralList.remove(0);
+		myGeneralList.remove(myGeneralList.size() - 1);
+
+		// Display the list.
+		System.out.println("\nNow with the first and last removed:");
+		displayList(myGeneralList);
+
+		// Add new elements to the beginning and end of the list.
+		myGeneralList.add(0, "dog");
+		myGeneralList.add("cat");
+
+		// Display the list.
+		System.out.println("\nNow with new first and last elements:");
+		displayList(myGeneralList);
+
+		keyboard.close();
+	}
+
+	/**
+	 * Displays the elements in a GeneralList
+	 * @param list
+	 */
+	public static void displayList(GeneralList<?> list)
+	{
+		for (int index = 0; index < list.size(); index++)
+		{
+			System.out.println("Index " + index + ": " +
+					list.get(index));
+		}
 	}
 }
