98d6c5ee7c01cef081879fec27a400603089f2c9
Adam Boas <anboas@gmail.com>
2014-11-30 13:55:00 -0500

    Added place holders for unimplemented methods

--- 2592/Assignments/assignment05/Deque.java
+++ 2592/Assignments/assignment05/Deque.java
@@ -4,6 +4,41 @@ import java.util.Iterator;
 
 public class Deque<E> implements Iterable<E> {
 
+	private int size; // Size of Deque list
+	private Node front; // First node in the list
+	private Node rear; // Last node in the list
+
+	private class Node {
+		E element;
+		Node next;
+		Node prev;
+	}
+
+	public boolean isEmpty() {
+		return front == null;
+	}
+
+	public void addFront(E element) {
+
+	}
+
+	public E removeFirst() {
+
+		return null;
+	}
+
+	public void addRear(E element) {
+
+	}
+
+	public E removeRear() {
+
+		return null;
+	}
+
+	public int getSize() {
+		return size;
+	}
 
 	@Override
 	public Iterator<E> iterator() {
