a8efce881943ea56202a695edd5cb175bae3e3dc
Adam Boas <anboas@gmail.com>
2014-11-30 13:43:10 -0500

    Added class definition

---
+++ 2592/Assignments/assignment05/Deque.java
@@ -0,0 +1,16 @@
+package assignment05;
+
+import java.util.Iterator;
+
+public class Deque<E> implements Iterable<E> {
+
+
+	@Override
+	public Iterator<E> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+}
